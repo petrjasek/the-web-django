942482ab5910d9ebddef41cae2e994db8d4e6050
petr.jasek <petr.jasek@sourcefabric.org>
2013-03-12 11:03:32 +0100

    Serve media files as static.

--- the_web/settings.py
+++ the_web/settings.py
@@ -53,12 +53,12 @@ app_root = os.path.dirname(os.path.realpath(__file__))
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(app_root, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(app_root), 'superdesk', 'static', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://localhost:8080/superdesk_web/the_web/media/'
+MEDIA_URL = '/static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
